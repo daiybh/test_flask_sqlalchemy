548d23059ee0cb0bf12a9aa561938b4a91914a52
Dai Li <daiybh@gmail.com>
2023-09-11 13:04:39 +0800

    mont logs

--- rundocker.sh
+++ rundocker.sh
@@ -1,1 +1,14 @@
- docker container run -d --name led_flask -p 1238:18080 --volume "$PWD/":/app python /bin/bash -c "chmod +x /app/docker_run.sh;/app/docker_run.sh"
+ 
+ appBasePath=/home/admin/myapps/ledpython/
+ appLogsPath=${appBasePath}/logs/
+ appUploadPath=${appBasePath}/upload/
+ mkdir ${appBasePath}
+ mkdir ${appLogsPath}
+ mkdir ${appUploadPath}
+ 
+ docker container run -d \
+        --name led_flask -p 1238:18080 \
+        --volume "$PWD/":/app  \
+        --volume "${appLogsPath}/":/app/logs  \
+        --volume "${appUploadPath}/":/app/upload  \
+        python /bin/bash -c "chmod +x /app/docker_run.sh;/app/docker_run.sh"
